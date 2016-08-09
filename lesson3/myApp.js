837938564a8f72272ca5beab2ebdef96a30e3ab0
hauru <hauruli@163.com>
2016-08-09 13:37:09 +0800

    完成 lesson 3 进阶部分

---
+++ lesson3/myApp.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var superagent = require("superagent");
+var cheerio = require("cheerio");
+var app = express();
+
+app.get('/', function (req, res, next) {
+    superagent.get("https://cnodejs.org")
+        .end(function (err, sres) {
+            if (err) {
+                return next(err);
+            }
+            var $ = cheerio.load(sres.text);
+            var items = [];
+            $("#topic_list .cell").each(
+                function (index, element) {
+                    var $element = $(element);
+                    items.push({
+                        "href": $element.find(".topic_title").attr("href"),
+                        "title": $element.find(".topic_title").attr("title"),
+                        "author": $element.find(".user_avatar.pull-left img").attr("title")
+                    });
+                }
+            );
+            res.send(items);
+        });
+});
+
+
+app.listen(3000, function () {
+    console.log('App listening on port 3000!');
+});
